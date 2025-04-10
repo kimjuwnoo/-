b9e952422844632261ad1b648b94530852cd134e
kimjuwnoo <akak1724@gmail.com>
2025-04-11 03:12:29 +0900

    Create BottomNav.tsx

---
+++ components/BottomNav.tsx
@@ -0,0 +1,22 @@
+// --- components/BottomNav.tsx ---
+
+import { Home, CalendarCheck, User } from "lucide-react";
+
+export default function BottomNav() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-md flex justify-around py-2">
+      <div className="flex flex-col items-center text-blue-500">
+        <Home size={20} />
+        <span className="text-xs">홈</span>
+      </div>
+      <div className="flex flex-col items-center text-gray-500">
+        <CalendarCheck size={20} />
+        <span className="text-xs">예약</span>
+      </div>
+      <div className="flex flex-col items-center text-gray-500">
+        <User size={20} />
+        <span className="text-xs">내정보</span>
+      </div>
+    </div>
+  );
+}
