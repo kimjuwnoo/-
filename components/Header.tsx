38fddd7685638ed3ee617e8bbf829c63be622ad9
kimjuwnoo <akak1724@gmail.com>
2025-04-11 03:11:37 +0900

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+// --- components/Header.tsx ---
+
+export default function Header() {
+  return (
+    <div className="text-white text-center">
+      <p className="text-sm">대한민국 대표</p>
+      <h1 className="text-3xl font-bold mt-1">타이어 중개 플랫폼!</h1>
+    </div>
+  );
+}
